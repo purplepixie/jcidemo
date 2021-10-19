f16193856b9c7c1fa8c22221f11661faf923a543
Rowan Adair <radair2842@gmail.com>
2021-10-19 11:35:49 +0100

    Test

---
+++ src/uk/ac/qub/eeecs/dave/uk/ac/qub/eeecs/dave/Calculator.java
@@ -0,0 +1,19 @@
+package uk.ac.qub.eeecs.dave;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a+b+1;
+	}
+	
+	public int sub(int a, int b) {
+		return a-b;
+	}
+
+	public double multiply(double a, double b) { return a * b ;}
+
+	public double divide(double a, double b) { return a / b ;}
+
+	public double remainder(double a, double b) {return a % b;}
+
+
+}
